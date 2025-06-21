7beedea84e7736f56988e770c308d516ac0d670d
flrngel <flrngel@gmail.com>
2025-06-21 15:19:21 -0700

    feat: aesthetic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,15 +14,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="min-h-screen font-body antialiased bg-background text-foreground flex flex-col">
         <AuthProvider>
-          {children}
+          <main className="flex-1 container mx-auto px-4 sm:px-6 lg:px-8">
+            {children}
+          </main>
           <Toaster />
         </AuthProvider>
       </body>
